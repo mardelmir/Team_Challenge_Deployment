58bd4405ad5712160420e81a37cd7fadd5f63839
María DM <mariad1996@gmail.com>
2024-12-02 20:09:45 +0100

    front: predict form test

--- app_hooks.py
+++ app_hooks.py
@@ -18,7 +18,19 @@ app.config['DEBUG'] = True
 def home():
     return render_template('index.html')
 
-# 
+# Predict
+@app.route('/api/v1/predict/', methods = ['POST', 'GET'])
+def make_prediction():
+    prediction = None
+    if request.method == 'POST':
+        
+        feature1 = float(request.form['feature1'])
+        feature2 = float(request.form['feature2'])
+
+        # prediction = model.predict(np.array([[feature1, feature2]])) 
+
+    return render_template('predict.html', prediction = prediction)
+        
 
 if __name__ == '__main__':
     app.run()
