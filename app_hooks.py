25312b852c986065b6b4c9248ed5894d29d5139b
María DM <mariad1996@gmail.com>
2024-12-06 17:42:03 +0100

    formatting

--- app_hooks.py
+++ app_hooks.py
@@ -4,8 +4,7 @@ import subprocess
 
 import numpy as np
 import pandas as pd
-
-from flask import Flask, render_template, jsonify, request, url_for, redirect
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from werkzeug.utils import secure_filename
 
 os.chdir(os.path.dirname(__file__))
@@ -16,28 +15,29 @@ ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1000 # limits uploads to 50KB per file
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1000  # limits uploads to 50KB per file
 
 
 # Landing page (endpoint /)
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
 
 # Predict Form
-@app.route('/api/v1/predict_form/', methods = ['GET'])
+@app.route('/api/v1/predict_form/', methods=['GET'])
 def get_prediction_form():
     return render_template('predictForm.html')
-    
 
-'''
+
+"""
 Test request for /api/v1/predict:
 http://127.0.0.1:5000/api/v1/predict?pressure=15&sun=60&mean_temp=80
-'''
+"""
+
 
 # Predict
-@app.route('/api/v1/predict/', methods = ['POST', 'GET'])
+@app.route('/api/v1/predict/', methods=['POST', 'GET'])
 def make_prediction():
     if request.method == 'POST':
         # Form data
@@ -46,57 +46,49 @@ def make_prediction():
         mean_temp = request.form['mean_temp']
 
         # This is a test to see that it retrieves form info correctly, the prediction would go here instead
-        
+
         # Redirection
-        return redirect(url_for('make_prediction', pressure = pressure, sun = sun, mean_temp = mean_temp))
+        return redirect(url_for('make_prediction', pressure=pressure, sun=sun, mean_temp=mean_temp))
 
     # If method = GET, get data from the query parameters
     pressure = request.args.get('pressure', None)
     sun = request.args.get('sun', None)
     mean_temp = request.args.get('mean_temp', None)
-    
-    # Prepare the result as a dictionary
-    result = {
-        'pressure': pressure,
-        'sun': sun,
-        'mean_temp': mean_temp
-    } if pressure and sun and mean_temp else None
-        
-    # Renders template with result
-    return render_template('predict.html', result = result)
 
-        
-        
+    # Prepare the result as a dictionary
+    result = {'pressure': pressure, 'sun': sun, 'mean_temp': mean_temp} if pressure and sun and mean_temp else None
+
+    # Renders template with result
+    return render_template('predict.html', result=result)
+
+
 # Forecast
-@app.route('/api/v1/forecast/', methods = ['POST', 'GET'])
+@app.route('/api/v1/forecast/', methods=['POST', 'GET'])
 def forecast():
     forecast = None
     if request.method == 'POST':
- 
-        feature1 = float(request.form['feature1'])
-        feature2 = float(request.form['feature2'])
+        _dummy_feat1 = float(request.form['feature1'])
+        _dummy_feat2 = float(request.form['feature2'])
 
-        # prediction = model.predict(np.array([[feature1, feature2]])) 
+        # prediction = model.predict(np.array([[feature1, feature2]]))
 
-    return render_template('forecast.html', forecast = forecast)
-
+    return render_template('forecast.html', forecast=forecast)
 
 
 # Update Data
-@app.route('/api/v1/update_data/', methods = ['POST', 'GET'])
+@app.route('/api/v1/update_data/', methods=['POST', 'GET'])
 def update_data():
     update_name = None
     if request.method == 'POST':
         f = request.files['newData']
         update_name = secure_filename(f.filename)
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], update_name))
-    
-    return render_template('updateData.html', update_name = update_name)
 
+    return render_template('updateData.html', update_name=update_name)
 
 
 # Retrain
-@app.route('/api/v1/retrain/', methods = ['POST', 'GET'])
+@app.route('/api/v1/retrain/', methods=['POST', 'GET'])
 def retrain_model():
     metrics = None
     if request.method == 'POST':
@@ -106,43 +98,46 @@ def retrain_model():
     # List of uploaded files to select from
     data_op = [file for file in os.listdir(UPLOAD_FOLDER) if file != 'dataset.zip']
     data_op = data_op if len(data_op) != 0 else None
-    
-    return render_template('retrain.html', metrics = metrics, data_op = data_op)
+
+    return render_template('retrain.html', metrics=metrics, data_op=data_op)
 
 
 # Webhook
-@app.route('/webhook', methods = ['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    # Ruta al repositorio donde se realizará el pull
+    # Path to the repository where git pull will be performed
     path_repo = '/home/mardelmir1/Team_Challenge_Deployment'
-    servidor_web = '/var/www/mardelmir1_pythonanywhere_com_wsgi.py'
+    web_server = '/var/www/mardelmir1_pythonanywhere_com_wsgi.py'
 
-    # Comprueba si la solicitud POST contiene datos JSON
+    # Checks if the POST request contains JSON data
     if request.is_json:
         payload = request.json
-        # Verifica si la carga útil (payload) contiene información sobre el repositorio
+        # Checks if the payload contains information about the repository
         if 'repository' in payload:
-            # Extrae el nombre del repositorio y la URL de clonación
+            # Extracts the repository name and cloning URL
             repo_name = payload['repository']['name']
             clone_url = payload['repository']['clone_url']
 
-            # Cambia al directorio del repositorio
+            # Changes to the repository directory
             try:
                 os.chdir(path_repo)
             except FileNotFoundError:
-                return jsonify({'message': 'El directorio del repositorio no existe!'}), 404
+                return jsonify({'message': 'Repository directory does not exist!'}), 404
 
-            # Realiza un git pull en el repositorio
+            # Does git pull on the repository
             try:
-                subprocess.run(['git', 'pull', clone_url], check = True)
-                subprocess.run(['touch', servidor_web], check = True)  # Trick to automatically reload PythonAnywhere WebServer
-                return jsonify({'message': f'Se realizó un git pull en el repositorio {repo_name}'}), 200
+                subprocess.run(['git', 'pull', clone_url], check=True)
+                subprocess.run(
+                    ['touch', web_server], check=True
+                )  # Trick to automatically reload PythonAnywhere WebServer
+                return jsonify({'message': f'A git pull was performed on repository {repo_name}'}), 200
             except subprocess.CalledProcessError:
-                return jsonify({'message': f'Error al realizar git pull en el repositorio {repo_name}'}), 500
+                return jsonify({'message': f'Error while performing git pull on repository {repo_name}'}), 500
         else:
-            return jsonify({'message': 'No se encontró información sobre el repositorio en la carga útil (payload)'}), 400
+            return jsonify({'message': 'No repository information found in the payload'}), 400
     else:
-        return jsonify({'message': 'La solicitud no contiene datos JSON'}), 400
+        return jsonify({'message': 'The request does not contain JSON data'}), 400
+
 
 if __name__ == '__main__':
     app.run()
--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,23 @@
-import numpy as np
-import pandas as pd
 import os
 import pickle as pkl
-
-from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_percentage_error
-from sklearn.model_selection import cross_validate, GridSearchCV
-from sklearn.pipeline import Pipeline
-
 from time import time
-
 from zipfile import ZipFile
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, root_mean_squared_error
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.pipeline import Pipeline
 
 # =====================================================================================================================================================================
 # Handle Data
+
 
 def process_data(X: pd.DataFrame) -> pd.DataFrame:
     X = X.dropna(axis=0).copy()
     X['pressure'] = X['pressure'] / 1000
     return X
+
 
 def load_zipped_data(zip_path: str) -> pd.DataFrame:
     with ZipFile(zip_path, 'r') as z:
@@ -25,19 +25,21 @@ def load_zipped_data(zip_path: str) -> pd.DataFrame:
             data = pd.read_csv(f)
     return data
 
+
 def save_to_zip(data: pd.DataFrame, zip_path: str, file_name: str):
     data.to_csv(file_name, index=False)
     with ZipFile(zip_path, 'w') as z:
         z.write(file_name, arcname=file_name)
     os.remove(file_name)
 
+
 # =====================================================================================================================================================================
 # Cross-Validate
 
 scaler = pkl.load(open('../transformers/scaler.pkl', 'rb'))
 
-def cross_validate_models(models: dict, X_train, y_train) -> pd.DataFrame:
 
+def cross_validate_models(models: dict, X_train, y_train) -> pd.DataFrame:
     model_names = []
     mse = []
     rmse = []
@@ -45,37 +47,34 @@ def cross_validate_models(models: dict, X_train, y_train) -> pd.DataFrame:
     pipes = {}
 
     for name, alg in models.items():
+        pipe = Pipeline(steps=[('scaler', scaler), ('regressor', alg)])
 
-        pipe = Pipeline(steps=[
-            ('scaler', scaler),
-            ('regressor', alg)
-        ])
+        CVresults = cross_validate(
+            pipe,
+            X_train,
+            y_train,
+            scoring=('neg_mean_squared_error', 'neg_root_mean_squared_error', 'neg_mean_absolute_percentage_error'),
+        )
 
-        CVresults = cross_validate(pipe, X_train, y_train, scoring=('neg_mean_squared_error','neg_root_mean_squared_error','neg_mean_absolute_percentage_error'))
-        
         model_names.append(name)
         mse.append(-np.mean(CVresults['test_neg_mean_squared_error']))
         rmse.append(-np.mean(CVresults['test_neg_root_mean_squared_error']))
         mape.append(-np.mean(CVresults['test_neg_mean_absolute_percentage_error']))
         pipes[name] = pipe
 
-    cvResultsDF = pd.DataFrame({
-        'Model': model_names,
-        'MSE': mse,
-        'RMSE': rmse,
-        'MAPE': mape
-    })
+    cvResultsDF = pd.DataFrame({'Model': model_names, 'MSE': mse, 'RMSE': rmse, 'MAPE': mape})
 
     return cvResultsDF, pipes
 
+
 # =====================================================================================================================================================================
 # Tune Hyperparams
+
 
 def tune_hyperparameters(pipelines: dict, param_grids: dict, X_train, y_train, cv_scoring: str) -> dict:
     tuned_models = {}
 
     for name, pipe in pipelines.items():
-
         params = param_grids.get(name)
         if params:
             print(f'Tuning {name} hyperparameters...')
@@ -86,11 +85,15 @@ def tune_hyperparameters(pipelines: dict, param_grids: dict, X_train, y_train, c
             end = time()
 
             tuning_time = end - start
-            time_message = (f'Tuning {name} took: {tuning_time:.3f} seconds' if tuning_time < 60 else f'Tuning {name} took: {tuning_time/60:.3f} minutes')
+            time_message = (
+                f'Tuning {name} took: {tuning_time:.3f} seconds'
+                if tuning_time < 60
+                else f'Tuning {name} took: {tuning_time/60:.3f} minutes'
+            )
 
             best = gs.best_estimator_
 
-            print(f'----Hyperparameter tuning complete ----')
+            print('----Hyperparameter tuning complete ----')
             print(time_message)
             if 'neg_' in cv_scoring:
                 print(f'Best Score: {-gs.best_score_:.5f}')
@@ -98,10 +101,10 @@ def tune_hyperparameters(pipelines: dict, param_grids: dict, X_train, y_train, c
                 print(f'Best Score: {gs.best_score_:.5f}')
             print(f'Best parameters:\n{gs.best_params_}')
             print()
-        
+
         else:
             print(f'No parameter grid found for {name}. Fitting model directly...')
-            
+
             start = time()
             cv = cross_validate(pipe, X_train, y_train, scoring=cv_scoring)
             pipe.fit(X_train, y_train)
@@ -109,7 +112,11 @@ def tune_hyperparameters(pipelines: dict, param_grids: dict, X_train, y_train, c
             end = time()
 
             tuning_time = end - start
-            time_message = (f'Fitting {name} took: {tuning_time:.3f} seconds' if tuning_time < 60 else f'Fitting {name} took: {tuning_time/60:.3f} minutes')
+            time_message = (
+                f'Fitting {name} took: {tuning_time:.3f} seconds'
+                if tuning_time < 60
+                else f'Fitting {name} took: {tuning_time/60:.3f} minutes'
+            )
             print(time_message)
 
             if 'neg_' in cv_scoring:
@@ -117,13 +124,15 @@ def tune_hyperparameters(pipelines: dict, param_grids: dict, X_train, y_train, c
             else:
                 print(f'Best Score: {np.mean(cv['test_score']):.5f}')
             print()
-        
+
         tuned_models[name] = best
 
     return tuned_models
 
+
 # =====================================================================================================================================================================
 # Evaluate on test set
+
 
 def test_evaluation(tuned_models: dict, X_train=None, y_train=None, X_test=None, y_test=None) -> pd.DataFrame:
     model_names = []
@@ -137,7 +146,6 @@ def test_evaluation(tuned_models: dict, X_train=None, y_train=None, X_test=None,
         tuned_models = {f'{tuned_models.steps[-1][1].__class__.__name__}': tuned_models}
 
     for name, model in tuned_models.items():
-
         refitted_models[name] = model.fit(X_train, y_train)
         y_preds = refitted_models[name].predict(X_test)
 
@@ -150,19 +158,18 @@ def test_evaluation(tuned_models: dict, X_train=None, y_train=None, X_test=None,
         rmse.append(rmse_score)
         mape.append(mape_score)
 
-    predResultsDF = pd.DataFrame({
-        'Model': model_names,
-        'MSE': mse,
-        'RMSE': rmse,
-        'MAPE': mape
-    })
+    predResultsDF = pd.DataFrame({'Model': model_names, 'MSE': mse, 'RMSE': rmse, 'MAPE': mape})
 
     return predResultsDF, refitted_models
+
 
 # =====================================================================================================================================================================
 # Save best model
 
-def save_best_model(test_resultsDF: pd.DataFrame, tuned_models: dict, selection_metric='', save_path='../models/best_model.pkl'):
+
+def save_best_model(
+    test_resultsDF: pd.DataFrame, tuned_models: dict, selection_metric='', save_path='../models/best_model.pkl'
+):
     if not isinstance(tuned_models, dict):
         best_model = tuned_models
     else:
